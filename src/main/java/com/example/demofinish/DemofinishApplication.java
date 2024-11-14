80eae6d0393b953ed3db35a49e0b84a6d2a1cd73
00Flaptzy <222151@unamba.edu.pe>
2024-11-14 15:46:56 -0500

    first commit

---
+++ src/main/java/com/example/demofinish/DemofinishApplication.java
@@ -0,0 +1,13 @@
+package com.example.demofinish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemofinishApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemofinishApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demofinish/DemofinishApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demofinish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemofinishApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
